2347701d591e6a8f358dc241cd1107e95cf91820
Sergey Shilin <shilinshtein@gmail.com>
2020-02-22 14:58:50 -0500

    Updated docstrings to use numpy style
    Replaced single quotes with double quotes

--- byter/reader.py
+++ byter/reader.py
@@ -1,235 +1,324 @@
+"""Byter read functions."""
+
 __all__ = [
-    'read_char',
-    'read_signed_char',
-    'read_unsigned_char',
-    'read_bool',
-    'read_short',
-    'read_unsigned_short',
-    'read_int',
-    'read_unsigned_int',
-    'read_long',
-    'read_unsigned_long',
-    'read_long_long',
-    'read_unsigned_long_long',
-    'read_float',
-    'read_double',
-    'read_string',
-    'read_array']
+    "read_char",
+    "read_signed_char",
+    "read_unsigned_char",
+    "read_bool",
+    "read_short",
+    "read_unsigned_short",
+    "read_int",
+    "read_unsigned_int",
+    "read_long",
+    "read_unsigned_long",
+    "read_long_long",
+    "read_unsigned_long_long",
+    "read_float",
+    "read_double",
+    "read_string",
+    "read_array"
+]
 
 import struct
+
 from .utils import get_type
 
 
 def read_char(data):
-    """Read 1 byte of data as `char`
+    """
+    Read 1 byte of data as `char`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        str -- Python string of length 1
+    Returns
+    -------
+    str
+        Python string of length of 1
     """
     s_type = get_type("char")
     return struct.unpack(s_type, data.read(1))[0]
 
 
 def read_signed_char(data):
-    """Read 1 byte of data as `signed char`
+    """
+    Read 1 byte of data as `signed char`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("signed_char")
     return struct.unpack(s_type, data.read(1))[0]
 
 
 def read_unsigned_char(data):
-    """Read 1 byte of data as `unsigned char`
+    """
+    Read 1 byte of data as `unsigned char`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("unsigned_char")
     return struct.unpack(s_type, data.read(1))[0]
 
 
 def read_bool(data):
-    """Read 1 byte of data as `bool` type
+    """
+    Read 1 byte of data as `bool` type.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        bool -- True or False
+    Returns
+    -------
+    bool
+        True or False
     """
     s_type = get_type("bool")
     return struct.unpack(s_type, data.read(1))[0]
 
 
 def read_short(data):
-    """Read 2 bytes of data as `short`
+    """
+    Read 2 bytes of data as `short`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("short")
     return struct.unpack(s_type, data.read(2))[0]
 
 
 def read_unsigned_short(data):
-    """Read 2 bytes of data as `unsigned short`
+    """
+    Read 2 bytes of data as `unsigned short`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("unsigned_short")
     return struct.unpack(s_type, data.read(2))[0]
 
 
 def read_int(data):
-    """Read 4 bytes of data as `int`
+    """
+    Read 4 bytes of data as `int`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer (int32)
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("int")
     return struct.unpack(s_type, data.read(4))[0]
 
 
 def read_unsigned_int(data):
-    """Read 4 bytes of data as `unsigned int`
+    """
+    Read 4 bytes of data as `unsigned int`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- Python integer (int32)
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("unsigned_int")
     return struct.unpack(s_type, data.read(4))[0]
 
 
 def read_long(data):
-    """Read 4 bytes of data as `long`.
-       It is highly recommended to use `read_int(data)` instead.
+    """
+    Read 4 bytes of data as `long`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    It is highly recommended to use `read_int(data)` instead.
 
-    Returns:
-        int -- Python integer (int32)
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
+
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("long")
     return struct.unpack(s_type, data.read(4))[0]
 
 
 def read_unsigned_long(data):
-    """Read 4 bytes of data as `unsigned long`
-       It is highly recommended to use `read_unsigned_int(data)` instead.
+    """
+    Read 4 bytes of data as `unsigned long`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    It is highly recommended to use `read_unsigned_int(data)` instead.
 
-    Returns:
-        int -- Python integer (int32)
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
+
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("unsigned_long")
     return struct.unpack(s_type, data.read(4))[0]
 
 
 def read_long_long(data):
-    """Read 8 bytes of data as `long long`
+    """
+    Read 8 bytes of data as `long long`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- 8-bytes python integer (int64)
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("long_long")
     return struct.unpack(s_type, data.read(8))[0]
 
 
 def read_unsigned_long_long(data):
-    """Read 8 bytes of data as `unsigned long long`
+    """
+    Read 8 bytes of data as `unsigned long long`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        int -- 8-bytes python integer (int64)
+    Returns
+    -------
+    int
+        Python integer
     """
     s_type = get_type("unsigned_long_long")
     return struct.unpack(s_type, data.read(8))[0]
 
 
 def read_float(data):
-    """Read 4 bytes of data as `float`
+    """
+    Read 4 bytes of data as `float`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        float -- 4-bytes python float (float32)
+    Returns
+    -------
+    float
+        Python float
     """
     s_type = get_type("float")
     return struct.unpack(s_type, data.read(4))[0]
 
 
 def read_double(data):
-    """Read 8 bytes of data as `double`
+    """
+    Read 8 bytes of data as `double`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
 
-    Returns:
-        float -- 8-bytes python float (float64)
+    Returns
+    -------
+    float
+        Python float
     """
     s_type = get_type("double")
     return struct.unpack(s_type, data.read(8))[0]
 
 
 def read_string(data, s_len):
-    """Read `s_len` bytes as `char[]`
+    """
+    Read `s_len` bytes as `char[]`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
-        s_len {int} -- Size of the string to read (# of bytes)
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
+    s_len : int
+        Size of the string to read (# of bytes)
 
-    Returns:
-        str -- Python string of length `s_len`
+    Returns
+    -------
+    str
+        Python string of length `s_len`
     """
     return struct.unpack("%ds" % s_len, data.read(s_len))[0].decode("utf-8")
 
 
 def read_array(data, size, c_type):
-    """Read `size` consequent elements, each of type `c_type`.
+    """
+    Read `size` consequent elements, each of type `c_type`.
 
-    Arguments:
-        data {io.BufferedReader} -- File open to read in binary mode
-        size {int} -- Number of elements to read
-        c_type {str} -- C-language type string (e.g. "unsigned_int")
+    Parameters
+    ----------
+    data : io.BufferedReader
+        File open to read in binary mode
+    size : int
+        Number of elements to read
+    c_type : str
+        C-language type string (e.g. "unsigned_int")
 
-    Returns:
-        list -- Python list of size `size`
+    Returns
+    -------
+    list
+        Python list of size `size`
     """
     s_type = get_type(c_type)
-    format_string = str(size) + s_type # e.g. 10i --> read 10 int's
+    format_string = str(size) + s_type  # e.g. 10i --> read 10 int's
     num_bytes_to_read = struct.calcsize(format_string)
 
     return struct.unpack(format_string, data.read(num_bytes_to_read))
--- byter/utils.py
+++ byter/utils.py
@@ -1,19 +1,28 @@
+"""Byter utilities."""
+
 from .constants import TYPES_TABLE
 
 
 def get_type(c_type):
-    """Get a format character from the C-language type string.
-       Examples:
-            get_type("unsigned_int") returns "I"
-            get_type("long") returns "l"
+    """
+    Get a format character from the C-language type string.
 
-        For more info refer to
-        https://docs.python.org/2/library/struct.html#format-characters
+    For more info refer to
+    https://docs.python.org/2/library/struct.html#format-characters
 
-    Arguments:
-        c_type {str} -- C-language type string (e.g. "unsigned_int")
+    Examples
+    --------
+        get_type("unsigned_int") returns "I"
+        get_type("long") returns "l"
 
-    Returns:
-        str -- a format character for `struct` library
+    Parameters
+    ----------
+    c_type : str
+        C-language type string (e.g. "unsigned_int")
+
+    Returns
+    -------
+    str
+        A format character for `struct` library
     """
     return TYPES_TABLE[c_type]
